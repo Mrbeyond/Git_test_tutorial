197df1ffb957bed48885649de8cdc77fa1638a4e
Mrbeyond <beyondbeyond602@gmail.com>
2021-05-11 10:41:28 +0100

    Third commit for function two on first.py file

--- First.py
+++ First.py
@@ -2,3 +2,9 @@ def one():
 	print("This is the first function to test git")
 	print("The function ends ")
 one()
+
+def two():
+	print("This is the second function to test git")
+	print("The second function ends ")
+
+two()
